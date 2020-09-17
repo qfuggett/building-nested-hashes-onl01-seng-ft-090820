5817bd88494114189c1e04c7b246d8a2e4cc2dbf
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 02:39:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,10 +3,8 @@ def third_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [ ]
    }, 
    :capulet => {
       :patriarch => {
