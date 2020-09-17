ce889d496d9517d72e73e8b9b2753baf617d67e8
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-17 02:37:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,7 +1,17 @@
 def first_challenge
    epic_tragedy = {
-     :montague ==> {},
+     :montague ==> {
+      :patriarch => { },
+      :matriarch => { },
+      :hero => { },
+      :hero_friends => [ ]
+     },
      
-     :capulet ==> {}
+     :capulet ==> {
+      :patriarch => { },
+      :matriarch => { },
+      :heroine => { },
+      :heroine_friends => [ ]
+     }
    }
 end
